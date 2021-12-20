fbebedb8c91cb158f952ed4e0b2e59d167079df1
lyubovmenshikova <dyachenko.lubov@yandex.ru>
2021-12-20 16:12:53 +0300

    Initial Commit

---
+++ Test - 2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test - 2.2
+//
+//  Created by Lyubov Menshikova on 20.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
